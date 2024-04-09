5260ee703170e9f17fea8d60afb03fd8c9400058
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-04-10 01:37:17 +0300

    add install files for libnvidia-encode1

--- gen-control-550.py
+++ gen-control-550.py
@@ -1417,6 +1417,26 @@ symbols-file-missing-build-depends-package-field"""
 
 # end of libnvidia-eglcore
 
+# libnvidia-encode1
+
+LIBNVIDIA_ENCODE1_INSTALL_FILE_PREQ =  """#! /usr/bin/dh-exec
+libnvidia-encode.so.{DRIVER_VERSION_FULL}	usr/lib/${{DEB_HOST_MULTIARCH}}/nvidia/current/"""
+
+LIBNVIDIA_ENCODE1_LINTIAN_FILE_PREQ = """# The NVIDIA license does not allow any form of modification.
+[i386]: binary-file-built-without-LFS-support
+[i386 ppc64el]: specific-address-in-shared-library
+hardening-no-bindnow
+hardening-no-fortify-functions
+
+# Lintian and debhelper disagree w.r.t. a library in a private directory.
+package-has-unnecessary-activation-of-ldconfig-trigger"""
+
+LIBNVIDIA_ENCODE1_LINKS_FILE_PREQ = """#! /usr/bin/dh-exec
+usr/lib/${{DEB_HOST_MULTIARCH}}/nvidia/current/libnvidia-encode.so.{DRIVER_VERSION_FULL}	usr/lib/${{DEB_HOST_MULTIARCH}}/nvidia/current/libnvidia-encode.so.1
+usr/lib/${{DEB_HOST_MULTIARCH}}/nvidia/current/libnvidia-encode.so.1    usr/lib/${{DEB_HOST_MULTIARCH}}/nvidia/current/libnvidia-encode.so"""
+
+# end of libnvidia-encode1
+
 ### End of Text Preq
 
 
@@ -1728,3 +1748,28 @@ with open(LIBNVIDIA_EGLCORE_LINTIAN_FILE_PATH, "w") as LIBNVIDIA_EGLCORE_LINTIAN
     LIBNVIDIA_EGLCORE_LINTIAN_FILE.write(LIBNVIDIA_EGLCORE_LINTIAN_FILECONTENT)
 
 # end of libnvidia-eglcore
+
+# libnvidia-encode1
+
+LIBNVIDIA_ENCODE1_INSTALL_FILE_PATH = 'libnvidia-encode1-' + DRIVER_VERSION_MAJOR + '.install'
+with open(LIBNVIDIA_ENCODE1_INSTALL_FILE_PATH, "w") as LIBNVIDIA_ENCODE1_INSTALL_FILE:
+    LIBNVIDIA_ENCODE1_INSTALL_FILECONTENT = LIBNVIDIA_ENCODE1_INSTALL_FILE_PREQ.format(
+        DRIVER_VERSION_FULL=DRIVER_VERSION_FULL,
+    )
+    LIBNVIDIA_ENCODE1_INSTALL_FILE.write(LIBNVIDIA_ENCODE1_INSTALL_FILECONTENT)
+
+LIBNVIDIA_ENCODE1_LINTIAN_FILE_PATH = 'libnvidia-encode1-' + DRIVER_VERSION_MAJOR + '.lintian-overrides'
+with open(LIBNVIDIA_ENCODE1_LINTIAN_FILE_PATH, "w") as LIBNVIDIA_ENCODE1_LINTIAN_FILE:
+    LIBNVIDIA_ENCODE1_LINTIAN_FILECONTENT = LIBNVIDIA_ENCODE1_LINTIAN_FILE_PREQ.format(
+        DRIVER_VERSION_FULL=DRIVER_VERSION_FULL,
+    )
+    LIBNVIDIA_ENCODE1_LINTIAN_FILE.write(LIBNVIDIA_ENCODE1_LINTIAN_FILECONTENT)
+
+LIBNVIDIA_ENCODE1_LINKS_FILE_PATH = 'libnvidia-encode1-' + DRIVER_VERSION_MAJOR + '.links'
+with open(LIBNVIDIA_ENCODE1_LINKS_FILE_PATH, "w") as LIBNVIDIA_ENCODE1_LINKS_FILE:
+    LIBNVIDIA_ENCODE1_LINKS_FILECONTENT = LIBNVIDIA_ENCODE1_LINKS_FILE_PREQ.format(
+        DRIVER_VERSION_FULL=DRIVER_VERSION_FULL,
+    )
+    LIBNVIDIA_ENCODE1_LINKS_FILE.write(LIBNVIDIA_ENCODE1_LINKS_FILECONTENT)
+
+# end of libnvidia-encode1
