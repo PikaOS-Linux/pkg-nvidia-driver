904377f00b44a10bfe5bbf6a317fffd11fe0959a
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-07-21 18:57:29 +0300

    update nvidia drivers

--- 555/config-i386.py
+++ 555/config-i386.py
@@ -2,7 +2,7 @@
 
 ### Basic configuration
 DRIVER_VERSION_MAJOR = "555"
-DRIVER_VERSION_FULL = "555.42.02"
+DRIVER_VERSION_FULL = "555.58.02"
 DEB_HOST_MULTIARCH = "i386-linux-gnu"
 ### End of Basic configuration
 
